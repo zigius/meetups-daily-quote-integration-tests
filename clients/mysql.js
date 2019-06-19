baad44d9720a907bb4854676d4820da921289d89
Rami Moshe <rms.rami.m@gmail.com>
2019-06-19 18:12:06 +0300

    master-removed echo and die statements, lolz.

--- clients/mysql.js
+++ clients/mysql.js
@@ -12,3 +12,14 @@ exports.cleanDb = async () => {
     await connection.queryAsync('TRUNCATE TABLE daily_quote');
     connection.end();
 };
+
+exports.getAllDailyQuotes = async () => {
+    const connection = mysql.createConnection(config.mysql);
+
+    connection.connect();
+    Promise.promisifyAll(connection);
+    const data = await connection.queryAsync('select * from daily_quote');
+    connection.end();
+
+    return data;
+};
