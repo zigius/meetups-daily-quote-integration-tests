80629e8096c32790950d2a622bd24c754633e075
Daniel Singer <zigius@gmail.com>
2019-06-24 17:19:45 +0300

    Revert "fuckup".

--- clients/localstack/sqs.sh
+++ clients/localstack/sqs.sh
@@ -4,4 +4,4 @@ export AWS_ACCESS_KEY_ID=foobar
 export AWS_SECRET_ACCESS_KEY=foobar
 export AWS_DEFAULT_REGION=us-east-1
 
-aws --endpoint=http://localstack:4576 sqs create-queue --queue-name "iron_queue"
+aws --endpoint=http://localstack:4576 sqs create-queue --queue-name "daily_quote_queue"
